7dd47504d26219d9c61ef4ca57cd3e8e995cf7c6
Samarskiy_A_S <samarskiy_a_s@MacBook-Pro-Sunlight.local>
2023-04-06 19:27:46 +0300

    cart.js and cart is working

--- static/assets/js/cart.js
+++ static/assets/js/cart.js
@@ -1,46 +1,87 @@
-var updateBtns = document.getElementsByClassName('update-cart')
-for (i = 0; i < updateBtns.length; i++) {
-    updateBtns[i].addEventListener('click', function () {
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('product ID:', productId, 'Action: ', action)
-        console.log("USER: ", user)
-        if (user == "AnonymousUser") {
-            console.log("Kullanıcı giriş yapmamış")
-        } else {
-            updateUserOrder(productId, action)
-        }
+// var updateBtns = document.getElementsByClassName('update-cart')
+// for (i = 0; i < updateBtns.length; i++) {
+//     updateBtns[i].addEventListener('click', function () {
+//         var productId = this.dataset.product
+//         var action = this.dataset.action
+//         console.log('product ID:', productId, 'Action: ', action)
+//         console.log("USER: ", user)
+//         if (user == "AnonymousUser") {
+//             console.log("Kullanıcı giriş yapmamış")
+//         } else {
+//             updateUserOrder(productId, action)
+//         }
+//     })
+
+//     function updateUserOrder(productId, action) {
+//         console.log("Giriş yapıldı ve sepete ekleniyor")
+//         var url = '/user/update_item/'
+//         fetch(url, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'X-CSRFToken': csrftoken
+//             },
+//             body: JSON.stringify({'productId': productId, 'action': action})
+//         })
+
+//             .then((response) => {
+//                 return response.json()
+//             })
+
+//             .then((data) => {
+//                 var message = document.createElement("div")
+//                 document.body.appendChild(message);
+//                 message.id = "product-added-succesfuly"
+//                 message.innerHTML = data;
+//                 setTimeout(() => {
+//                     message.classList.add("closing-effect")
+//                 }, 3000)
+//                 setTimeout(function () {
+//                     message.remove()
+//                 }, 4000);
+
+//             })
+//     }
+// }
+
+function clickCartButton(target) {
+  //   var target = event.target;
+  var productId = target.dataset.product;
+  var action = target.dataset.action;
+  console.log("product ID:", productId, "Action: ", action);
+  console.log("USER: ", user);
+  if (user == "AnonymousUser") {
+    console.log("Kullanıcı giriş yapmamış");
+  } else {
+    updateUserOrder(productId, action);
+  }
+}
+
+function updateUserOrder(productId, action) {
+  console.log("Giriş yapıldı ve sepete ekleniyor");
+  var url = "/user/update_item/";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken,
+    },
+    body: JSON.stringify({ productId: productId, action: action }),
+  })
+    .then((response) => {
+      return response.json();
     })
 
-    function updateUserOrder(productId, action) {
-        console.log("Giriş yapıldı ve sepete ekleniyor")
-        var url = '/user/update_item/'
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': csrftoken
-            },
-            body: JSON.stringify({'productId': productId, 'action': action})
-        })
-
-            .then((response) => {
-                return response.json()
-            })
-
-            .then((data) => {
-                var message = document.createElement("div")
-                document.body.appendChild(message);
-                message.id = "product-added-succesfuly"
-                message.innerHTML = data;
-                setTimeout(() => {
-                    message.classList.add("closing-effect")
-                }, 3000)
-                setTimeout(function () {
-                    message.remove()
-                }, 4000);
-
-
-            })
-    }
+    .then((data) => {
+      var message = document.createElement("div");
+      document.body.appendChild(message);
+      message.id = "product-added-succesfuly";
+      message.innerHTML = data;
+      setTimeout(() => {
+        message.classList.add("closing-effect");
+      }, 3000);
+      setTimeout(function () {
+        message.remove();
+      }, 4000);
+    });
 }
